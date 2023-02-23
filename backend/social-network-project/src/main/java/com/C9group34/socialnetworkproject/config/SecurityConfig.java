f48f1d61f0fc01960bd900fefac41370435345b7
Gabusy07 <100946627+Gabusy07@users.noreply.github.com>
2023-02-23 20:37:47 -0300

    comienzo con seguridad dependencias y clase config

---
+++ backend/social-network-project/src/main/java/com/C9group34/socialnetworkproject/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.C9group34.socialnetworkproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+}
